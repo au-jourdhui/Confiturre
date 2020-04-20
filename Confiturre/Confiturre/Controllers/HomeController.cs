9839882f66e58a6441748e2d1d8dc11d1f197ed4
au-jourdhui <petitefille0910@gmail.com>
2020-04-20 15:19:32 +0300

    Layouts

--- Confiturre/Confiturre/Controllers/HomeController.cs
+++ Confiturre/Confiturre/Controllers/HomeController.cs
@@ -10,18 +10,22 @@ namespace Confiturre.Controllers
     {
         public IActionResult Index()
         {
+            ViewBag.ActiveName = "About";
             return View();
         }
         public IActionResult News()
         {
+            ViewBag.ActiveName = "News";
             return View();
         }
         public IActionResult Portfolio()
         {
+            ViewBag.ActiveName = "Portfolio";
             return View();
         }
         public IActionResult Contacts()
         {
+            ViewBag.ActiveName = "Contacts";
             return View();
         }
     }
